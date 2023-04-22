19fa9f765b9c917aa77a73e6546c8dd11188e43c
SEFAT x AYAN <114663032+SEFAT-AYAN@users.noreply.github.com>
2023-04-22 20:57:30 +0400

    Update SPIDER.py

--- SPIDER.py
+++ SPIDER.py
@@ -7,7 +7,7 @@ bitt=platform.architecture()[0]
 if bitt=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import RN.mahadi
+    import MAHADI.mahadi
 elif bitt=="32bit":
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
