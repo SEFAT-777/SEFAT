e52b866501801d02ff22435d0805ef4ed7a8ca92
SEFAT x MAHADI <114663032+SEFAT-MAHADI@users.noreply.github.com>
2023-05-01 10:42:08 +0400

    Update SPIDER.py

--- SPIDER.py
+++ SPIDER.py
@@ -5,11 +5,12 @@ import os,platform,time
 bitt=platform.architecture()[0]
  
 if bitt=="64bit":
-    os.system('xdg open https://facebook.com/groups/463607967795665/')
+    os.system('xdg-open https://facebook.com/groups/463607967795665/')
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import RN.mahadi
 elif bitt=="32bit":
+    os.system('xdg-open https://facebook.com/groups/463607967795665/')
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import RN32.mahadi
