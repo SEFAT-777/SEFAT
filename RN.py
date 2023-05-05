005c1c107ac58fe7dd0b57e1c509ab1ad6824b66
SEFAT x MAHADI <114663032+SEFAT-MAHADI@users.noreply.github.com>
2023-05-05 22:17:43 +0400

    b

--- RN.py
+++
@@ -1,7 +0,0 @@
-import os
-from platform import architecture
-if architecture()[0]=='64bit':os.system('git pull;chmod +x Mahadi;./Mahadi')
-
-elif architecture()[0]=='32bit':os.system('git pull;chmod +x Sefat;./Sefat')
-
-else:exit('\033[1;31m\n Sorry unknown device not support ')
