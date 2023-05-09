e6295edf41d77fd9d7a9ce3697489265f4dfe7d6
SEFAT x MAHADI <114663032+SEFAT-MAHADI@users.noreply.github.com>
2023-05-09 05:23:41 +0400

    Update SPIDER.py

--- SPIDER.py
+++ SPIDER.py
@@ -5,9 +5,9 @@ from platform import architecture
 os.system('xdg-open https://facebook.com/groups/463607967795665/')
 if architecture()[0]=='64bit':os.system('git pull;chmod +x Mahadi;./Mahadi')
  
-elif architecture()[0]=='32bit':os.system('git pull;chmod +x Sefat;./Sefat')
+#elif architecture()[0]=='32bit':os.system('git pull;chmod +x Sefat;./Sefat')
  
-else:exit('\033[1;31m\n Sorry unknown device not support ')
+else:exit('\033[1;31m\n Sorry you device 32 bit not support ')
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
 #                  • MAHADI HASAN AFRIDI •
