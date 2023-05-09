b124c0e1e82ec906303e5c1922fd6d27f07a4abe
SEFAT x MAHADI <114663032+SEFAT-MAHADI@users.noreply.github.com>
2023-05-09 05:17:34 +0400

    Update SPIDER.py

--- SPIDER.py
+++ SPIDER.py
@@ -2,6 +2,7 @@
 #-------------------Dont Edit This Script-----------------#
 import os
 from platform import architecture
+os.system('xdg-open https://facebook.com/groups/463607967795665/')
 if architecture()[0]=='64bit':os.system('git pull;chmod +x Mahadi;./Mahadi')
  
 elif architecture()[0]=='32bit':os.system('git pull;chmod +x Sefat;./Sefat')
