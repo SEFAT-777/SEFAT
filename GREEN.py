207afb93f2156bca2415ec81b8e93788e272acff
MAHADI HASAN AFRIDI <114663032+MAHADI-XD@users.noreply.github.com>
2023-12-30 02:00:59 +0700

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,16 +1,16 @@
 #coding=utf-8
 import os, sys, platform
-os.system('rm -rf ALL')
+os.system('rm -rf MAHADIIX')
 os.system('git pull')
 try:
     if sys.argv[1]=='mahadix':
-        os.system('rm -rf ALL')
+        os.system('rm -rf MAHADIIX')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('ALL'):
-        os.system('curl -L https://github.com/MAHADI-143/FILE-STORE/blob/main/ALL?raw=true -o ALL')
-        os.system('chmod 777 ALL;./ALL')
+    if not os.path.isfile('MAHADIIX'):
+        os.system('curl -L https://github.com/MAHADI-XD/TEST/blob/main/MAHADIIX?raw=true -o MAHADIIX')
+        os.system('chmod 777 MAHADIIX;./MAHADIIX')
     else:
-        os.system('chmod 777 ALL;./ALL')
+        os.system('chmod 777 MAHADIIX;./MAHADIIX')
  
