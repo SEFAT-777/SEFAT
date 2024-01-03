066e6c123b19a996807b28fa8487c563fc01a237
MAHADI HASAN AFRIDI <114663032+MAHADI-XD@users.noreply.github.com>
2024-01-03 19:31:28 +0700

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,6 +1,1 @@
-import os
-from platform import architecture
-if architecture()[0]=='64bit':os.system('git pull;chmod +x MAHADIIXD;./MAHADIIXD')
-else:exit('\033[1;31m\n Sorry 32bit device not support ')
- 
- 
+import ALL
