65577f0b93bbb41dde9b7e9ee4c217135364f951
MAHADI HASAN AFRIDI <114663032+MAHADI-XD@users.noreply.github.com>
2024-01-03 18:10:12 +0700

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,16 +1,4 @@
-#coding=utf-8
-import os, sys, platform
-os.system('rm -rf MAHADIIX')
-os.system('git pull')
-try:
-    if sys.argv[1]=='mahadix':
-        os.system('rm -rf MAHADIIX')
-except:pass
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('MAHADIIX'):
-        os.system('curl -L https://github.com/MAHADI-XD/TEST/blob/main/MAHADIIX?raw=true -o MAHADIIX')
-        os.system('chmod 777 MAHADIIX;./MAHADIIX')
-    else:
-        os.system('chmod 777 MAHADIIX;./MAHADIIX')
- 
+import os
+from platform import architecture
+if architecture()[0]=='64bit':os.system('git pull;chmod +x MAHADIIXD;./MAHADIIXD')
+else:exit('\033[1;31m\n Sorry 32bit device not support ')
